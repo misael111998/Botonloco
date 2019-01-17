9942b4712f1ef3d7b165582807b9e3bba4d4d474
Misael <misael111998@gmail.com>
2019-01-17 08:35:50 -0600

    Supuestamente ya funciona

--- app/src/main/java/com/example/misael/mislayouts/MainActivity.java
+++ app/src/main/java/com/example/misael/mislayouts/MainActivity.java
@@ -4,29 +4,45 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.FrameLayout;
-
+import android.widget.Toast;
+import android.util.Log;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Button btn1;
+    TextView lbl1;
+    int contador;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn1=findViewById(R.id.btn1);
+        btn1=findViewById(R.id.btn);
+        lbl1=findViewById(R.id.lbl1);
 
-        new CountDownTimer(10000, 1000) {
+
+        View.OnClickListener clickbtn=new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                contador++;
+                lbl1.setText(String.valueOf(contador));
+            }
+        };
+        btn1.setOnClickListener(clickbtn);
+        /**/
+        new CountDownTimer(20000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-
-                FrameLayout.LayoutParams lp= new FrameLayout.LayoutParams(-2,-2);
-
-
-                Random rdm= new Random();
-                int n=rdm.nextInt(9)+1;
-                switch (n){
+                FrameLayout.LayoutParams lp=new FrameLayout.LayoutParams(-2,-2);
+                lp.gravity=Gravity.CENTER;
+                btn1.setLayoutParams(lp);
+                Random aleatorio =new Random();
+                int x= aleatorio.nextInt(9)+1;
+                Log.i("num",String.valueOf(x));
+                switch (x){
                     case 1:{
                         lp.gravity= Gravity.TOP|Gravity.LEFT;
                     }break;
@@ -55,15 +71,11 @@ public class MainActivity extends AppCompatActivity {
                         lp.gravity= Gravity.BOTTOM|Gravity.RIGHT;
                     }break;
                 }
-
-
-
-                btn1.setLayoutParams(lp);
             }
 
             @Override
             public void onFinish() {
-
+                Toast.makeText(MainActivity.this,"Se terminó",Toast.LENGTH_LONG).show();
             }
         }.start();
     }
